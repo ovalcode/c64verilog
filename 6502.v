0409593fa77e5988f74fea218273fd15fdbf8a08
Johan Steenkamp <ovalcode@users.noreply.github.com>
2017-05-06 19:45:59 +0200

    new file:   6502.v 	new file:   6502_tb.v

---
+++ 6502.v
@@ -0,0 +1,90 @@
+module _6502(di, clk, reset, we, ab);
+
+  parameter WIDTH = 8;
+  parameter RESET_0 = 8'd0,
+            //RESET_1 = 8'd1;
+            DECODE = 8'd1;
+
+  reg [7:0] state;
+
+  input [WIDTH-1 : 0] di;
+  output reg [15:0] ab;
+  input 	       clk, reset;
+  output we;
+  reg [7:0] acc;
+  reg load_acc;
+
+//  reg [WIDTH-1 : 0]   out;
+  reg we;
+  reg [15:0] pc;
+  reg [15:0] pc_temp;
+  reg [15:0] pc_inc;
+  wire 	       clk, reset;
+
+  //state 0 -> reset everything pc, ab to 0
+  // everything except state 0 -> increment pc by one
+
+  always @(posedge clk)
+  begin
+    pc <= pc_temp + pc_inc;
+    //ab <= pc;
+    $display("Hello %d, %d", pc, clk);
+    $display("Hello2 %d, %d", di, clk);
+  end
+
+  //change pc_temp and pc_inc
+  always @*
+    case(state)
+      RESET_0: begin 
+                 pc_temp = 0;
+                 pc_inc = 0;
+               end
+      //everything starts happening at RESET_1 -> should actually be DECODE??
+    endcase
+
+  //address generator
+  always @*
+    case(state)
+      default: ab = pc;
+    endcase
+
+  //set register
+  always @(posedge clk)
+  if (state == DECODE)
+    case(di)
+      8'ha9: load_acc <= 1;
+      default: load_acc <= 0;
+    endcase
+  
+  always @(posedge clk)
+  if (load_acc)
+    acc <= di;
+
+   //write block parsing instruction codes
+  always @*
+    case(state)
+      DECODE: case(di)
+                8'ha9: state <= DECODE;//what must be done with LDA#imm?;
+                //for decode increment PC
+                //this should form part of state machine
+              endcase
+    endcase
+
+  //state machine
+  always @(posedge clk or posedge reset)
+  if (reset)
+  begin
+      state <= RESET_0;
+      //pc <= 0;
+  end
+  else case (state)
+      DECODE: case (di)
+                8'ha9: state <= DECODE;//do something//set next state
+              endcase
+      RESET_0: state <= DECODE;
+      //RESET_1: state <= RESET_2;
+      //RESET_2: state <= RESET_3;
+      //RESET_3: state <= RESET_4;
+  endcase
+
+endmodule // counter
---
+++ 6502_tb.v
@@ -0,0 +1,72 @@
+module test;
+  //iverilog -o my_design  6502_tb.v 6502.v
+
+  /* Make a reset that pulses once. */
+  reg reset = 0;
+  reg [7:0] ram [0:27];
+  initial begin
+     ram[0] = 8'ha9;
+     ram[1] = 8'd20;
+     ram[2] = 8'd3;
+     ram[3] = 8'd3;
+     ram[4] = 8'd3;
+     ram[5] = 8'd3;
+     ram[6] = 8'd3;
+     ram[7] = 8'd3;
+     ram[8] = 8'd3;
+     ram[9] = 8'd3;
+     ram[10] = 8'd3;
+     ram[11] = 8'd3;
+     ram[12] = 8'd3;
+     ram[13] = 8'd3;
+     ram[14] = 8'd3;
+     ram[15] = 8'd3;
+     ram[16] = 8'd9;
+     ram[17] = 8'd9;
+     ram[18] = 8'd9;
+//$display("Hello daar");
+     ram[19] = 8'd9;
+     ram[20] = 8'd9;
+     ram[21] = 8'd9;
+     ram[22] = 8'd9;
+     ram[23] = 8'd17;
+     ram[24] = 8'd18;
+     ram[25] = 8'd19;
+     ram[26] = 8'd20;
+     ram[27] = 8'd21;
+
+     #10 reset = 1;
+     #10 reset = 0;
+     #100 $stop;
+
+     //# 17 reset = 1;
+     //# 11 reset = 0;
+     //# 29 reset = 1;
+     //# 11 reset = 0;
+     //# 100 $stop;
+  end
+
+  /* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #1 clk = !clk;
+
+  //wire [7:0] value;
+  //reg di = 0;
+  wire [7:0] di;
+  wire [15:0] ab;
+  wire we;
+  assign di = ram[ab];
+  _6502 c1 (di, clk, reset, we, ab);
+
+
+  //always @(posedge clk)
+  //begin
+  //  pc <= pc + 1;
+  //  $display("Hello %d, %d", pc, clk);
+  //end
+
+
+  initial
+     $monitor("At time %t, value = %h (%0d)",
+              $time, clk, clk);
+endmodule // test
